7862e9550bec5cac588b02f29ed50f4ee03f5bfa
Brian <riceb53@gmail.com>
2018-07-09 21:03:43 -0500

    add middle name and bio

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -13,6 +13,8 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
       first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      bio: params[:bio],
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number]
@@ -28,6 +30,8 @@ class Api::ContactsController < ApplicationController
 
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
+    @contact.bio = params[:bio] || @contact.bio
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
 
--- app/views/api/contacts/_contact.json.jbuilder
+++ app/views/api/contacts/_contact.json.jbuilder
@@ -1,6 +1,8 @@
 json.id contact.id
 json.first_name contact.first_name
+json.middle_name contact.middle_name
 json.last_name contact.last_name
+json.bio contact.bio
 json.email contact.email
 json.phone_number contact.phone_number
 json.formatted do
